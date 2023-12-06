4c5494f2bfbe457888f8e0de421e0f4ba841d331
Mário Monteiro <marioammonteiro@gmail.com>
2023-12-06 23:31:04 +0000

    add extras to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,31 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+	// prevent vite from obscuring rust errors
+
+	// extras to default config, ftom tauri sidecar app
+
+	clearScreen: false,
+	// tauri expects a fixed port, fail if that port is not available
+	server: {
+		// default
+		// port: 5173,
+		strictPort: true,
+		cors: true,
+	},
+	// to make use of `TAURI_DEBUG` and other env variables
+	// https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
+	envPrefix: ["VITE_", "TAURI_"],
+	build: {
+		// Tauri supports es2021
+		target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
+		// don't minify for debug builds
+		minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+		// produce sourcemaps for debug builds
+		sourcemap: !!process.env.TAURI_DEBUG,
+	},
 };
 
 export default config;
